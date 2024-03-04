3c74005fd38aef1e7a38aba49350868fe846d56d
arvend <arvendrajan100@gmail.com>
2024-03-04 16:08:18 +0800

    initial commit

---
+++ src/main/java/com/medicare/cartservice/MedicareCartServiceApplication.java
@@ -0,0 +1,13 @@
+package com.medicare.cartservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicareCartServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicareCartServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medicare/cartservice/MedicareCartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicare.cartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicareCartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
